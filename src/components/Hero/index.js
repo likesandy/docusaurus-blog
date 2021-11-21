ebe1058c6f21b0eef7378e418cf2aef36a22d2aa
codertao <1445709137@qq.com>
2021-11-21 23:27:41 +0800

    test

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,16 +3,8 @@ import React from "react";
 import { useTrail, animated } from "react-spring";
 import Translate, { translate } from "@docusaurus/Translate";
 
-import Button from "../Button";
 
 import HeroMain from "./img/hero_main.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faWeixin,
-} from "@fortawesome/free-brands-svg-icons";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-
 
 import styles from "./styles.module.css";
 
@@ -41,10 +33,10 @@ function Hero() {
           </span>
         </animated.div>
         <animated.p style={animatedTexts[1]}>
-          <p>   本仓库建立的初衷是为了记录一路走来学习的计算机专业知识，方便之后复习与查看。起于此，但不止于此，在不断的摸索和完善，勤能补拙，相信一点点的积累最后汇聚成海！希望我的这个小小的计划，可以帮助到实力强大的你！止于至善
-            <br />
-            爱分享，爱生活！在我眼里，你永远是不一样的烟火！觉得还不错的话，记得好好学习吖！
-          </p>
+          {`本仓库建立的初衷是为了记录一路走来学习的计算机专业知识，方便之后复习与查看。起于此，但不止于此，在不断的摸索和完善，勤能补拙，相信一点点的积累最后汇聚成海！希望我的这个小小的计划，可以帮助到实力强大的你！止于至善
+          
+          爱分享，爱生活！在我眼里，你永远是不一样的烟火！觉得还不错的话，记得好好学习吖！
+          `}
         </animated.p>
         <animated.p style={animatedTexts[3]}>
           <Translate id="homepage.qqgroup1" description="qq group1">
