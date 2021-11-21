b879107c5fb4b5d4d6d590d79859d358fa534e89
codertao <1445709137@qq.com>
2021-11-21 19:34:29 +0800

    button测试修复

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -18,13 +18,12 @@ const StyledButton = styled.button`
   background: linear-gradient(
     90deg,
     var(--ifm-color-primary) 11.3%,
-    var(--ifm-color-primary-light) 161.54%
+    #20b7d9 161.54%
   );
   box-shadow: 0px 0px 32px rgba(0, 105, 165, 0.35);
   border-radius: 7px;
   font-family: "Yuanti SC", "Youyuan", "You Yuan", "幼圆", "PingFang SC",
     "Microsoft Yahei", Helvetica, sans-serif;
-
   :hover {
     color: white;
     text-decoration: none;
